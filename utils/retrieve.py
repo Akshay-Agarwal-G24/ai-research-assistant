5568eaadc09f685a6ec9aa7083cc3d15efdaf53b
Akshay Agarwal <akshaykragarwal23@gmail.com>
2026-06-07 19:05:01 +0530

    Add metadata retrieval support

--- utils/retrieve.py
+++ utils/retrieve.py
@@ -10,12 +10,18 @@ def retrieve_chunks(query_embedding, top_k=3):
 
     results = collection.query(
         query_embeddings=[query_embedding.tolist()],
-        n_results=top_k
+        n_results=top_k,
+        include=[
+            "documents",
+            "distances",
+            "metadatas"
+        ]
     )
 
     #return results
     return {
         "chunks": results["documents"][0],
         "distances": results["distances"][0],
-        "ids": results["ids"][0]
+        "ids": results["ids"][0],
+        "metadata": results["metadatas"][0]
     }
