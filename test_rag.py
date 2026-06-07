26d5d7b8086c55131e84180acb93cc2ea8811e13
Akshay Agarwal <akshaykragarwal23@gmail.com>
2026-06-07 19:14:17 +0530

    Add source citations to RAG responses

--- test_rag.py
+++ test_rag.py
@@ -1,7 +1,19 @@
 from utils.rag import ask_question
 
-question = "What is Docker?"
+""" question = "What is Docker?"
 
 answer = ask_question(question)
 
-print(answer)
+print(answer) """
+
+question = input("Ask a question: ")
+
+response = ask_question(question)
+
+print("\nAnswer:\n")
+print(response["answer"])
+
+print("\nSources:")
+
+for source in response["sources"]:
+    print(f"- {source}")
